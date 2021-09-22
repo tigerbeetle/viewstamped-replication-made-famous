a476b3b71db0f6564907dbdaa29a9cab313b2167
SarahCoilAccount <sarah@coil.com>
2021-09-22 13:35:27 +0200

    Linting

--- vopr.bat
+++ vopr.bat
@@ -1,25 +1,25 @@
 @echo off
 
 :: Install Zig 0.8.0 if a zig folder does not already exist:
-IF NOT EXIST zig\ (
-    CALL scripts/install_zig.bat latest
-    ECHO Running the TigerBeetle VOPR for the first time...
-    ECHO Visit https://www.tigerbeetle.com
+if not exist zig\ (
+    call scripts/install_zig.bat 0.8.0
+    echo Running the TigerBeetle VOPR for the first time...
+    echo Visit https://www.tigerbeetle.com
 )
 
-:: If a seed is provided as an argument then replay the seed, otherwise test a 1,000 seeds:
-IF NOT "%~1"=="" (
+:: If a seed is provided as an argument then replay the seed, otherwise test 1,000 seeds:
+if not "%~1"=="" (
     :: Build in fast ReleaseSafe mode if required, useful where you don't need debug logging:
-    IF "%~2"=="-OReleaseSafe" (
-        ECHO Replaying seed %~1 in ReleaseSafe mode...
-        CALL zig run src/simulator.zig -OReleaseSafe -- %~1
-    ) ELSE (
-        ECHO Replaying seed %~1 in Debug mode with full debug logging enabled...
-        CALL zig run src/simulator.zig -ODebug -- %~1
+    if "%~2"=="-OReleaseSafe" (
+        echo Replaying seed %~1 in ReleaseSafe mode...
+        call zig run src/simulator.zig -OReleaseSafe -- %~1
+    ) else (
+        echo Replaying seed %~1 in Debug mode with full debug logging enabled...
+        call zig run src/simulator.zig -ODebug -- %~1
     )
-) ELSE (
-    CALL zig build-exe src/simulator.zig -OReleaseSafe
+) else (
+    call zig build-exe src/simulator.zig -OReleaseSafe
     for /L %%i in (1,1,1000) do (
-        CALL simulator
+        call simulator
     )
 )
